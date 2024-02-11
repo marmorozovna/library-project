c194e84dc2ba27e09319232d22cca424cce9b9b0
marmorozovna <marina.morozevich@gmail.com>
2024-02-11 21:23:29 +0300

    feature AuthorRestControllerTest week 24

--- library-project/src/test/java/ru/itgirl/libraryproject/controller/AuthorRestControllerTest.java
+++ library-project/src/test/java/ru/itgirl/libraryproject/controller/AuthorRestControllerTest.java
@@ -1,21 +1,26 @@
 package ru.itgirl.libraryproject.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.itgirl.libraryproject.model.dto.AuthorCreateDTO;
 import ru.itgirl.libraryproject.model.dto.AuthorDTO;
+import ru.itgirl.libraryproject.model.dto.AuthorUpdateDTO;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc (addFilters = false)
+@AutoConfigureMockMvc(addFilters = false)
 public class AuthorRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
     @Test
     public void testGetAuthorById() throws Exception {
         Long authorId = 1L;
@@ -30,4 +35,100 @@ public class AuthorRestControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(authorDto.getName()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(authorDto.getSurname()));
     }
+
+    @Test
+    public void testGetAuthorByName() throws Exception {
+        Long authorId = 1L;
+        String authorName = "Александр";
+        AuthorDTO authorDto = new AuthorDTO();
+        authorDto.setId(authorId);
+        authorDto.setName(authorName);
+        authorDto.setSurname("Пушкин");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/author").param("name", authorName))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(authorDto.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(authorDto.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(authorDto.getSurname()));
+    }
+
+    @Test
+    public void testGetAuthorByNameV2() throws Exception {
+        Long authorId = 1L;
+        String authorName = "Александр";
+        AuthorDTO authorDto = new AuthorDTO();
+        authorDto.setId(authorId);
+        authorDto.setName(authorName);
+        authorDto.setSurname("Пушкин");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/author/v2").param("name", authorName))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(authorDto.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(authorDto.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(authorDto.getSurname()));
+    }
+
+    @Test
+    public void testGetAuthorByNameV3() throws Exception {
+        Long authorId = 1L;
+        String authorName = "Александр";
+        AuthorDTO authorDto = new AuthorDTO();
+        authorDto.setId(authorId);
+        authorDto.setName(authorName);
+        authorDto.setSurname("Пушкин");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/author/v3").param("name", authorName))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(authorDto.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(authorDto.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(authorDto.getSurname()));
+    }
+
+    @Test
+    public void testCreateAuthor() throws Exception {
+        AuthorCreateDTO authorCreateDto = new AuthorCreateDTO();
+        authorCreateDto.setName("Александр");
+        authorCreateDto.setSurname("Пушкин");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/author/create").accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(authorCreateDto)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(authorCreateDto.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(authorCreateDto.getSurname()));
+    }
+
+    @Test
+    public void testUpdateAuthor() throws Exception {
+        Long authorId = 1L;
+        AuthorUpdateDTO authorUpdateDTO = new AuthorUpdateDTO();
+        authorUpdateDTO.setId(authorId);
+        authorUpdateDTO.setName("Александр");
+        authorUpdateDTO.setSurname("Пушкин");
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/author/update").accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(authorUpdateDTO)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(authorUpdateDTO.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(authorUpdateDTO.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(authorUpdateDTO.getSurname()));
+    }
+
+    @Test
+    public void testDeleteAuthor() throws Exception {
+        Long authorId = 1L;
+        mockMvc.perform(MockMvcRequestBuilders.delete("/author/delete/{id}", authorId))
+                .andExpect(status().isOk());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
